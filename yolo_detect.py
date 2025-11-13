5b03270e69821c5378133a550c0aca3681cc8614
Daniyal <74063814+Danirali@users.noreply.github.com>
2025-11-13 13:30:03 +0000

    Add files via upload

--- yolo_detect.py
+++ yolo_detect.py
@@ -5,9 +5,22 @@ import cv2
 model = YOLO('yolov8n.pt')  # Use yolov8s.pt, yolov8m.pt etc. based on needs
 
 # Load image
-for i in range(1, 5):
-    image_path = f'capture_{i}.jpg'
-    img = cv2.imread(image_path)
+def scan(image):
+    if not image:
+        for i in range(1, 4):
+            image_path = f'capture_{i}.jpg'
+            img = cv2.imread(image_path)
+
+            # Run inference
+            results = model(img)
+
+            # Visualize results on the image
+            annotated_img = results[0].plot()  # Plots all detections
+
+            cv2.imshow("YOLOv8 Person Detection", annotated_img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    img = cv2.imread(image)
 
     # Run inference
     results = model(img)
@@ -18,3 +31,6 @@ for i in range(1, 5):
     cv2.imshow("YOLOv8 Person Detection", annotated_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+scan('')
+# scan('test.jpg')
